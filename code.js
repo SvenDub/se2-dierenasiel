78506e811ff8b97d4a92724a8f596024d65cec1d
Sven Dubbeld <sven@bitsensor.io>
2016-04-21 21:47:08 +0200

    Validate input

--- code.js
+++ code.js
@@ -1,12 +1,59 @@
 function OnReady() {
     var name = $('#add-name');
-    var hint = $('#add-name-hint');
+    var nameHint = $('#add-name-hint');
+    var idHint = $('#add-id-hint');
+
     name.focus(function () {
-        show(hint);
+        show(nameHint);
+        hide(idHint);
     });
     name.blur(function () {
-        hide(hint);
+        hide(nameHint);
     });
+
+    $('#add-form').submit(OnSubmit);
+}
+
+function OnSubmit(event) {
+    var nameHint = $('#add-name-hint');
+    var idHint = $('#add-id-hint');
+
+    hide(nameHint);
+    hide(idHint);
+
+    var animal = {
+        'species': $('input[name="add-type"]:checked').val(),
+        'name': $('#add-name').val(),
+        'age': $('#add-age').val(),
+        'regnr': $('#add-id').val(),
+        'reserved': $('#add-reserved').is(':checked')
+    };
+
+    if (!(animal.species === 'cat'
+        || animal.species === 'dog')) {
+        event.preventDefault();
+        return;
+    }
+
+    if (animal.name.length === 0) {
+        event.preventDefault();
+        show(nameHint);
+        return;
+    }
+
+
+    if (animal.age.length > 0 && animal.age < 0) {
+        event.preventDefault();
+        return;
+    }
+
+    if (animal.regnr.length > 0 && !animal.regnr.startsWith('0')) {
+        event.preventDefault();
+        show(idHint);
+        return;
+    }
+
+    console.log(animal);
 }
 
 function show(el) {
